8790635973ec3228a9f80e45ae0f73ff5c1aeb20
Adam Undus <adam.undus@neuraflash.com>
2020-10-07 20:55:50 -0400

    functional prototype

---
+++ ebooks/book.js
@@ -0,0 +1,57 @@
+(function ($) {
+    const addbtn = $('#add-to-booklist');
+    const url = window.location && window.location.toString().split('/');
+    const bookId = url[url.length - 1].match(/(\d+)/)[0];
+
+    if (JSON.parse(localStorage.getItem('booklist'))[bookId]) {
+        // book already in list
+        addbtn.html('Remove book from My Book List')
+        addbtn.css("background-color", "indianred");
+        addbtn.addClass('book-in-list')
+    }
+
+
+    addbtn.on('click', function (event) {
+        if (addbtn.hasClass('book-in-list')) {
+            removeBook();
+            return;
+        }
+        // add book to list
+        let author = '';
+        $("tbody>tr").each(function (index) {
+            const row = $(this)
+            const rowTitle = row.find('th:first').html();
+            if (rowTitle === 'Author') {
+                const tableRow = row.find('td:first')
+                author = tableRow.find('a:first').html();
+            }
+        });
+        const bookData = {
+            title: $('h1').html(),
+            bookId: bookId,
+            imgSrc: $('img.cover-art').attr('src'),
+            author: author,
+            url: window.location
+        };
+
+        const newList = { ...JSON.parse(localStorage.getItem('booklist')) };
+        newList[bookId] = bookData;
+        addbtn.addClass('book-in-list')
+        addbtn.html('Remove book from My Book List')
+        addbtn.css("background-color", "indianred")
+
+        localStorage.setItem('booklist', JSON.stringify(newList));
+        console.log(localStorage.getItem('booklist'));
+    });
+
+    function removeBook() {
+        const newlist = JSON.parse(localStorage.getItem('booklist'))
+        delete newlist[bookId]
+        localStorage.setItem('booklist', JSON.stringify(newlist));
+        console.log(localStorage.getItem('booklist'));
+        addbtn.removeClass('book-in-list')
+        addbtn.html('Add to My Book List')
+        addbtn.css("background-color", "cornflowerblue")
+    }
+
+})(jQuery);
